8d3641828c133e0c054249889dfd88cbdbb0fd05
Eslam Hany Mohamed Riad <113130802+EslamHany2002@users.noreply.github.com>
2023-12-14 20:29:31 +0200

    add edit button and some space

--- lib/presentation/UI/Home/Taps/Profile/Profile.dart
+++ lib/presentation/UI/Home/Taps/Profile/Profile.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:rickman/presentation/UI/Edit%20Profile/EditProfile.dart';
@@ -11,27 +11,37 @@ class Profile extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 15,
+      ),
       body: Stack(
         children: [
           ListView(
             children: [
+
               const SizedBox(height: 300,),
 
-
               CustomButton(
-                title: "feedback",
+                title: "Edit",
+                action: (){      Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => EditProfile()));},
+                icon: EvaIcons.edit_2_outline,
+              ),
+              const SizedBox(height: 20,),
+              CustomButton(
+                title: "Feedback",
                 action: (){},
                 icon: EvaIcons.smiling_face,
               ),
               const SizedBox(height: 20,),
               CustomButton(
-                title: "aboutUs",
+                title: "AboutUs",
                 action: (){},
                 icon: Bootstrap.info_circle,
               ),
               const SizedBox(height: 20,),
               CustomButton(
-                title: "signOut",
+                title: "SignOut",
                 action: (){},
                 icon: Bootstrap.box_arrow_in_right,
                 color: Colors.red,
@@ -40,9 +50,6 @@ class Profile extends StatelessWidget{
             ],
           ),
           UserProfileDataWidget(
-
-            buttonTitle: "edit",
-            buttonAction: (){Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => EditProfile()));},
             isEn: true,
           ),
         ],
