5c2a8c349d2331cfa515c2fdc6714ffef3b63d6c
Eslam Hany Mohamed Riad <113130802+EslamHany2002@users.noreply.github.com>
2024-01-15 01:30:27 +0200

    Add intro second screen

---
+++ lib/presentation/UI/Intro/Taps/Intro_Screen 2/Intro_Screen 2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IntroSecondScreen extends StatelessWidget {
+  const IntroSecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.pink[100],
+    );
+  }
+}
