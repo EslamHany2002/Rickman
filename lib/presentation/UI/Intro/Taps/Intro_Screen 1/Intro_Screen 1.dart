7e5a672ef9061eb7ceef3557b2aa30b54040d4b5
Eslam Hany Mohamed Riad <113130802+EslamHany2002@users.noreply.github.com>
2024-01-15 17:40:35 +0200

    Add first intro screen

--- lib/presentation/UI/Intro/Taps/Intro_Screen 1/Intro_Screen 1.dart
+++ lib/presentation/UI/Intro/Taps/Intro_Screen 1/Intro_Screen 1.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class IntroFirstScreen extends StatelessWidget {
   const IntroFirstScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue[100],
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            children: [
+
+
+              SvgPicture.asset("Assets/SVG/CT scan-amico.svg"),
+              const Text("Welcome to Rickman",style: TextStyle(fontSize: 25),),
+
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Smart Scan , ",style: TextStyle(fontSize: 22),),
+
+                  Text("Brighter Futures",style: TextStyle(fontSize: 22,color:Colors.blue),),
+                ],
+              ),
+
+
+
+              // Text("data")
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
