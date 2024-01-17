1be04c826d32db676c55337b8db3fcb17646b6df
Eslam Hany Mohamed Riad <113130802+EslamHany2002@users.noreply.github.com>
2024-01-17 15:58:26 +0200

    add padding

--- lib/presentation/UI/Home/Taps/Profile/Widgets/UserProfileDataWidget.dart
+++ lib/presentation/UI/Home/Taps/Profile/Widgets/UserProfileDataWidget.dart
@@ -7,90 +7,93 @@ class UserProfileDataWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 190,
-      padding: const EdgeInsets.all(25),
-      decoration: BoxDecoration(
-          // color: Colors.black54,
-          borderRadius: BorderRadius.only(
-        bottomLeft: isEn ? const Radius.circular(80) : const Radius.circular(0),
-        bottomRight:
-            isEn ? const Radius.circular(0) : const Radius.circular(80),
-      )),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // user image
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                decoration: BoxDecoration(
+    return Padding(
+      padding: const EdgeInsets.only(top:30),
+      child: Container(
+        height: 190,
+        padding: const EdgeInsets.all(25),
+        decoration: BoxDecoration(
+            // color: Colors.black54,
+            borderRadius: BorderRadius.only(
+          bottomLeft: isEn ? const Radius.circular(80) : const Radius.circular(0),
+          bottomRight:
+              isEn ? const Radius.circular(0) : const Radius.circular(80),
+        )),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // user image
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomRight: const Radius.circular(80),
+                        bottomLeft: const Radius.circular(80),
+                        topLeft: isEn
+                            ? const Radius.circular(15)
+                            : const Radius.circular(80),
+                        topRight: isEn
+                            ? const Radius.circular(80)
+                            : const Radius.circular(15),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.25),
+                            offset: const Offset(0, 4),
+                            blurRadius: 10)
+                      ]),
+                  child: ClipRRect(
                     borderRadius: BorderRadius.only(
                       bottomRight: const Radius.circular(80),
                       bottomLeft: const Radius.circular(80),
                       topLeft: isEn
                           ? const Radius.circular(15)
-                          : const Radius.circular(80),
+                          : const Radius.circular(15),
                       topRight: isEn
-                          ? const Radius.circular(80)
+                          ? const Radius.circular(15)
                           : const Radius.circular(15),
                     ),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black.withOpacity(0.25),
-                          offset: const Offset(0, 4),
-                          blurRadius: 10)
-                    ]),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                    bottomRight: const Radius.circular(80),
-                    bottomLeft: const Radius.circular(80),
-                    topLeft: isEn
-                        ? const Radius.circular(15)
-                        : const Radius.circular(15),
-                    topRight: isEn
-                        ? const Radius.circular(15)
-                        : const Radius.circular(15),
-                  ),
-                  child: Image.asset(
-                    "Assets/Images/me.jpg",
-                    fit: BoxFit.cover,
-                    width: 125,
-                    height: 125,
+                    child: Image.asset(
+                      "Assets/Images/me.jpg",
+                      fit: BoxFit.cover,
+                      width: 125,
+                      height: 125,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-          // user name , email and edit profile button
-          const Expanded(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Spacer(),
+              ],
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            // user name , email and edit profile button
+            const Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Spacer(),
 
-              Text(
-                "Eslam",
-                style: TextStyle(
-                  // color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+                Text(
+                  "Eslam",
+                  style: TextStyle(
+                    // color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  maxLines: 1,
                 ),
-                maxLines: 1,
-              ),
-              // Spacer(),
-              FittedBox(
-                  child: Text(
-                "eslamhanyriad@gmail.com",
-              )),
-              Spacer(),
-            ],
-          ))
-        ],
+                // Spacer(),
+                FittedBox(
+                    child: Text(
+                  "eslamhanyriad@gmail.com",
+                )),
+                Spacer(),
+              ],
+            ))
+          ],
+        ),
       ),
     );
   }
