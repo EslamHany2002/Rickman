edff68a34df67ce6fddb2178e6ec3259b164de46
Eslam Hany Mohamed Riad <113130802+EslamHany2002@users.noreply.github.com>
2024-01-17 15:53:42 +0200

    add third intro page

--- lib/presentation/UI/Intro/Taps/Intro_Screen 3/Intro_Screen 3.dart
+++ lib/presentation/UI/Intro/Taps/Intro_Screen 3/Intro_Screen 3.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class IntroThirdScreen extends StatelessWidget {
   const IntroThirdScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      color: Colors.deepPurple[100],
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          children: [
+            SvgPicture.asset("Assets/SVG/Doctors-pana.svg"),
+            const Text(
+              "Lets",
+              style: TextStyle(fontSize: 25),
+            ),
+            // const Text("Save your future",style: TextStyle(fontSize: 23),),
+
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Save your ",
+                  style: TextStyle(fontSize: 23),
+                ),
+                Text(
+                  "Future",
+                  style: TextStyle(fontSize: 23, color: Colors.blue),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
