a525e7b93b17750d2c5abd176b33b1cbc6448bb9
Eslam Hany Mohamed Riad <113130802+EslamHany2002@users.noreply.github.com>
2024-01-17 15:52:38 +0200

    add second intro page

--- lib/presentation/UI/Intro/Taps/Intro_Screen 2/Intro_Screen 2.dart
+++ lib/presentation/UI/Intro/Taps/Intro_Screen 2/Intro_Screen 2.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class IntroSecondScreen extends StatelessWidget {
   const IntroSecondScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.pink[100],
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          children: [
+            SvgPicture.asset("Assets/SVG/brain chemistry-amico.svg"),
+            const Text(
+              "We will detect ",
+              style: TextStyle(fontSize: 25),
+            ),
+            const Text(
+              "if you have brain tumor or not",
+              style: TextStyle(fontSize: 23),
+            ),
+
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "By Scan , ",
+                  style: TextStyle(fontSize: 22),
+                ),
+                Text(
+                  "Your MRI Image",
+                  style: TextStyle(fontSize: 22, color: Colors.blue),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
