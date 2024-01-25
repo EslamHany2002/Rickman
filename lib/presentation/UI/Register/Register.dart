041eb485893dd8df5e3103e859e3f6088ef49019
Eslam Hany Mohamed Riad <113130802+EslamHany2002@users.noreply.github.com>
2024-01-25 20:52:05 +0200

    add media query

--- lib/presentation/UI/Register/Register.dart
+++ lib/presentation/UI/Register/Register.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:rickman/presentation/UI/ExtraInfo/ExtraInfoView.dart';
@@ -6,88 +5,124 @@ import 'package:rickman/presentation/UI/Login/Login.dart';
 import 'package:rickman/presentation/UI/Widgets/CustomPasswordTextFormField.dart';
 import 'package:rickman/presentation/UI/Widgets/CustomTextFormField.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
   TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
+        physics: const BouncingScrollPhysics(),
         child: Padding(
-          padding: EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              const SizedBox(
-                height: 100,
-              ),
-              // El3b logo
-              Image.asset(
+              Padding(
+                padding: const EdgeInsets.only(top: 75),
+                child: Image.asset(
                   "Assets/Images/Horizontal_Brain_Cancer_Logo.png",
                   height: 100,
                 ),
-
-              const SizedBox(
-                height: 80,
+              ),
+              SizedBox(
+                height: size.height * 0.05,
               ),
               Form(
                   child: Column(
                 children: [
-                  CustomTextFormField(label: "Name", icon: EvaIcons.person,validator: nameValidation,),
-                  const SizedBox(
-                    height: 20,
+                  CustomTextFormField(
+                    label: "Name",
+                    icon: EvaIcons.person,
+                    validator: nameValidation,
                   ),
-                  CustomTextFormField(label: "Email", icon: EvaIcons.email, validator: emailValidation,),
-                  const SizedBox(
-                    height: 20,
+                  SizedBox(
+                    height: size.height * 0.025,
                   ),
-                  CustomPasswordTextFormField(label: "password", icon: EvaIcons.lock,validator: passwordValidation,),
-                  const SizedBox(
-                    height: 20,
+                  CustomTextFormField(
+                    label: "Email",
+                    icon: EvaIcons.email,
+                    validator: emailValidation,
                   ),
-                  CustomPasswordTextFormField(label: "Confirm password", icon: EvaIcons.lock,validator: passwordConfirmationValidation,),
-                  const SizedBox(
-                    height: 30,
+                  SizedBox(
+                    height: size.height * 0.025,
+                  ),
+                  CustomPasswordTextFormField(
+                    label: "password",
+                    icon: EvaIcons.lock,
+                    validator: passwordValidation,
+                  ),
+                  SizedBox(
+                    height: size.height * 0.025,
+                  ),
+                  CustomPasswordTextFormField(
+                    label: "Confirm password",
+                    icon: EvaIcons.lock,
+                    validator: passwordConfirmationValidation,
+                  ),
+                  SizedBox(
+                    height: size.height * 0.025,
                   ),
                   // Login Button
                   ElevatedButton(
                       style: ButtonStyle(
-                        foregroundColor: MaterialStateProperty.all(Colors.black),
-                        backgroundColor: MaterialStateProperty.all(Colors.black),
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.black),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.black),
                         elevation: MaterialStateProperty.all(0),
                         shape: MaterialStateProperty.all(RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10),
                         )),
                         textStyle: MaterialStateProperty.all(const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white)),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.white)),
                       ),
-                      onPressed: () {Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => ExtraInfoView()));},
-                      child: Row(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (_) => ExtraInfoView()));
+                      },
+                      child: const Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: Text("Register" ,style:  TextStyle(color: Colors.white , fontSize: 25),),
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              "Register",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 25),
+                            ),
                           ),
                         ],
-                      )
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
+                      )),
+
                   // Create Account Button
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
+                      const Text(
                         "Already Have Account?",
-                        style: TextStyle(color: Colors.black , fontSize: 16),
+                        style: TextStyle(color: Colors.black, fontSize: 16),
                       ),
                       TextButton(
-                        onPressed: (){Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => Login()));},
-                        child: Text(
+                        onPressed: () {
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (_) => const Login()));
+                        },
+                        child: const Text(
                           "Login",
-                          style: TextStyle(color: Colors.black45 , fontSize: 15 , decoration: TextDecoration.underline),
+                          style: TextStyle(
+                              color: Colors.black45,
+                              fontSize: 15,
+                              decoration: TextDecoration.underline),
                         ),
                       )
                     ],
@@ -116,9 +151,9 @@ class Register extends StatelessWidget {
     if (input.isEmpty) {
       return "emailCantBeEmpty";
     } else if (!RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+"
-    r"@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
-    r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
-    r"{0,253}[a-zA-Z0-9])?)*$")
+            r"@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+            r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+            r"{0,253}[a-zA-Z0-9])?)*$")
         .hasMatch(input)) {
       return "Enter a Valid Email";
     }
