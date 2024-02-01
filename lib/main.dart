3005e5d8e4a67c62e561a6394b79b88bcae2670a
Eslam Hany Mohamed Riad <113130802+EslamHany2002@users.noreply.github.com>
2024-02-01 17:11:39 +0200

    connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
 import 'presentation/UI/Splach/SplachScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid?
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyA3U0uOGFASKgtYUetd6aP1cW3cgHH67m4",
+      appId: "1:356610947569:android:4be40bad971416c59aac50",
+      messagingSenderId: "356610947569",
+      projectId: "rickman-de167",
+    ),
+  ):
+  await Firebase.initializeApp();
   runApp( MyApp());
 }
 
