d81d746ebc042805cb133b56ca0a8293be84ff17
Eslam Hany Mohamed Riad <113130802+EslamHany2002@users.noreply.github.com>
2024-03-13 17:50:52 +0200

    change ui and call some info

--- lib/presentation/UI/Home/Taps/Profile/Profile.dart
+++ lib/presentation/UI/Home/Taps/Profile/Profile.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
@@ -16,168 +17,235 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   final currentUser = FirebaseAuth.instance.currentUser!;
+  String name = '';
+  String email = '';
+  String age = '';
+  String gender = '';
+  String phone = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getUserInfo();
+  }
+
+  Future<void> getUserInfo() async {
+    final currentUser = FirebaseAuth.instance.currentUser!;
+    if (currentUser != null) {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUser.uid)
+          .get();
+      setState(() {
+        name = userDoc['First Name'];
+        email = userDoc['email'];
+        age = userDoc['Age'];
+        gender = userDoc['gender'];
+        phone = userDoc['phone'];
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: Container(
-                height: 190,
-                padding: const EdgeInsets.all(25),
-                decoration: BoxDecoration(
-                    // color: Colors.black54,
-                    borderRadius: BorderRadius.only(
-                  bottomLeft: const Radius.circular(80),
-                  bottomRight: const Radius.circular(0),
-                )),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // user image
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                bottomRight: const Radius.circular(80),
-                                bottomLeft: const Radius.circular(80),
-                                topLeft: const Radius.circular(15),
-                                topRight: const Radius.circular(80),
-                              ),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Colors.black.withOpacity(0.25),
-                                    offset: const Offset(0, 4),
-                                    blurRadius: 10)
-                              ]),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.only(
-                              bottomRight: const Radius.circular(80),
-                              bottomLeft: const Radius.circular(80),
-                              topLeft: const Radius.circular(15),
-                              topRight: const Radius.circular(15),
-                            ),
-                            child: Image.asset(
-                              "Assets/Images/me.jpg",
-                              fit: BoxFit.cover,
-                              width: 125,
-                              height: 125,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    // user name , email and edit profile button
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Spacer(),
-
-                        Text(
-                          currentUser.displayName!,
-                          style: TextStyle(
-                            // color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          maxLines: 1,
-                        ),
-                        // Spacer(),
-                        FittedBox(
-                            child: Text(
-                          currentUser.email!,
-                        )),
-                        Spacer(),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Container(
+              height: 190,
+              padding: const EdgeInsets.all(25),
+              decoration: BoxDecoration(
+                  // color: Colors.black54,
+                  borderRadius: BorderRadius.only(
+                bottomLeft: const Radius.circular(80),
+                bottomRight: const Radius.circular(0),
+              )),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(2.0),
-                    child: Text(
-                      "Theme",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                    ),
+                  SizedBox(width: MediaQuery.of(context).size.width*0.28,),
+                  // user image
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 140,
+                        height: 140,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            bottomRight: const Radius.circular(80),
+                            bottomLeft: const Radius.circular(80),
+                            topLeft: const Radius.circular(15),
+                            topRight: const Radius.circular(80),
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.25),
+                              offset: const Offset(0, 4),
+                              blurRadius: 10,
+                            )
+                          ],
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.only(
+                            bottomRight: const Radius.circular(80),
+                            bottomLeft: const Radius.circular(80),
+                            topLeft: const Radius.circular(15),
+                            topRight: const Radius.circular(15),
+                          ),
+                          child: Image.asset(
+                            "Assets/Images/me.jpg",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  ThemeSwitch(),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  // user name , email and edit profile button
+                  // Column(
+                  //   crossAxisAlignment: CrossAxisAlignment.start,
+                  //   children: [
+                  //     Spacer(),
+                  //
+                  //     Text(
+                  //       name,
+                  //       style: TextStyle(
+                  //         // color: Colors.white,
+                  //         fontSize: 24,
+                  //         fontWeight: FontWeight.bold,
+                  //       ),
+                  //     ),
+                  //     // Spacer(),
+                  //     Text(
+                  //       email,
+                  //     ),
+                  //     Spacer(),
+                  //   ],
+                  // )
                 ],
               ),
             ),
-            SizedBox(
-              height: 15,
-            ),
-            CustomButton(
-              title: "Edit",
-              action: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => const EditProfile()));
-              },
-              icon: EvaIcons.edit_2_outline,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomButton(
-              title: "Feedback",
-              action: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => const FeedbackPage()));
-              },
-              icon: EvaIcons.smiling_face_outline,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomButton(
-              title: "AboutUs",
-              action: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => const AboutUs()));
-              },
-              icon: Bootstrap.info_circle,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomButton(
-              title: "SignOut",
-              action: () {
-                AuthenticationServices().signOut(context);
-                Navigator.pushReplacement(
-                    context, MaterialPageRoute(builder: (_) => Login()));
-              },
-              icon: Bootstrap.box_arrow_in_right,
-              color: Colors.red,
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 25,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  FittedBox(
+                      fit: BoxFit.contain,
+                      child: Text(
+                        "Name : $name",
+                        style: TextStyle(fontSize: 19),
+                      )),
+                  FittedBox(
+                      fit: BoxFit.contain,
+                      child: Text(
+                        "Email : $email",
+                        style: TextStyle(fontSize: 19),
+                      )),
+                  FittedBox(
+                      fit: BoxFit.contain,
+                      child: Text(
+                        "Phone : $phone",
+                        style: TextStyle(fontSize: 19),
+                      )),
+                  FittedBox(
+                      fit: BoxFit.contain,
+                      child: Text(
+                        "Age : $age",
+                        style: TextStyle(fontSize: 19),
+                      )),
+                  FittedBox(
+                      fit: BoxFit.contain,
+                      child: Text(
+                        "Gender : $gender",
+                        style: TextStyle(fontSize: 19),
+                      )),
+                ],
+              ),
+            ],
+          ),
 
-        // UserProfileDataWidget(
-        //   isEn: true,
-        // ),
+          // Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          //   child: Row(
+          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //     children: [
+          //       Padding(
+          //         padding: const EdgeInsets.all(2.0),
+          //         child: Text(
+          //           "Theme",
+          //           style:
+          //               TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          //         ),
+          //       ),
+          //       ThemeSwitch(),
+          //     ],
+          //   ),
+          // ),
+          SizedBox(
+            height: 15,
+          ),
+          CustomButton(
+            title: "Edit",
+            action: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (_) => UpdateProfileScreen()));
+            },
+            icon: EvaIcons.edit_2_outline,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomButton(
+            title: "Feedback",
+            action: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (_) => const FeedbackPage()));
+            },
+            icon: EvaIcons.smiling_face_outline,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomButton(
+            title: "AboutUs",
+            action: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => const AboutUs()));
+            },
+            icon: Bootstrap.info_circle,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomButton(
+            title: "SignOut",
+            action: () {
+              AuthenticationServices().signOut(context);
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (_) => Login()));
+            },
+            icon: Bootstrap.box_arrow_in_right,
+            color: Colors.red,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+        ],
       ),
     );
   }
