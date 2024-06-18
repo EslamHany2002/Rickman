9a40121328ce4bd99d57f11d0e3b1d3c971f4a36
Eslam Hany Mohamed Riad <113130802+EslamHany2002@users.noreply.github.com>
2024-06-18 04:59:45 +0300

    update main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'dart:io' show Platform;
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'presentation/UI/Splach/SplachScreen.dart';
+import 'package:rickman/presentation/UI/CheckUser/CheckUser.dart';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:rickman/presentation/UI/Home/HomePage.dart';
+
+import 'presentation/UI/SplachScreen/SplachScreen.dart';
 
 
 void main() async{
@@ -14,6 +19,7 @@ void main() async{
       appId: "1:356610947569:android:4be40bad971416c59aac50",
       messagingSenderId: "356610947569",
       projectId: "rickman-de167",
+      storageBucket: "rickman-de167.appspot.com",
     ),
   ):
   await Firebase.initializeApp();
@@ -24,9 +30,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: SplachScreen()
+        home: SplashScreen()
     );
   }
 }
+
 
 
